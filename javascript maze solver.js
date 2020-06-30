ca907a8f85f33b3601087149e0d1494b76cf4198
WolfgangNS <WolfgangStark7@gmail.com>
2020-06-30 18:48:11 -0400

    Update javascript maze solver.js
    Make "map" variables global, change variable names. The script should use a lot less memory now.

--- javascript maze solver.js
+++ javascript maze solver.js
@@ -6,8 +6,7 @@ for(i=0;i<list.length;i++){
 if(list[i][0]==item[0]&&list[i][1]==item[1]){return true};
 }return false};
 
-var maze = 
-{map:
+map =
 [
 [0,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
 [0,1,1,0,1,1,0,1,1,1,1,0,1,1,1,0,1,0,0,1,0,1,0],
@@ -33,17 +32,19 @@ var maze =
 [0,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,1,0,1,0,1,1,0],
 [0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0]
 ],
-h:function(){return this.map.length},
-w:function(){return this.map[0].length},
-end:[15,22],
+h=function(){return map.length};
+w=function(){return map[0].length}; //do these need to be functions?
+end=[15,22];
 
+var maze = 
+{
 solve:function(r,c){
 if(d<=0){console.log("recursion error!");return []}else{d--;}
-if(c>this.w()-1|r>this.h()-1|c<0|r<0){return false};
-if(this.map[r][c]!=0){return false};
+if(c>w()-1|r>h()-1|c<0|r<0){return false};
+if(map[r][c]!=0){return false};
 if(contains(p,[r,c])){return false};
 p = p.concat([[r,c]]); //concat or push??
-if(contains([this.end],[r,c])){return [[r,c]]};
+if(contains([end],[r,c])){return [[r,c]]};
 if(a=this.solve(r,c+1)){return [[r,c]].concat(a)};
 if(a=this.solve(r+1,c)){return [[r,c]].concat(a)};
 if(a=this.solve(r,c-1)){return [[r,c]].concat(a)};
@@ -58,15 +59,15 @@ var table = document.getElementById("table");
 while(table.firstChild){table.removeChild(table.firstChild);}
 var row = [];
 
-for(q=0;q<maze.h();q++){
+for(q=0;q<h();q++){
 row[q] = document.createElement("tr");
 table.appendChild(row[q]);
-for(r=0;r<maze.w();r++){row[q].appendChild(document.createElement("td"))};
+for(r=0;r<w();r++){row[q].appendChild(document.createElement("td"))};
 }
 
 //SETTING WALL COLOR
-for(v=0;v<maze.h();v++){for(w=0;w<maze.w();w++){
-if(maze.map[v][w]==1){document.getElementsByTagName("tr")[v].getElementsByTagName("td")[w].style.backgroundColor="black"}
+for(y=0;y<h();y++){for(x=0;x<w();x++){
+if(map[y][x]==1){document.getElementsByTagName("tr")[y].getElementsByTagName("td")[x].style.backgroundColor="black"}
 }}
 
 }//end script()
@@ -75,7 +76,7 @@ if(maze.map[v][w]==1){document.getElementsByTagName("tr")[v].getElementsByTagNam
 function solve(){
 var solution = maze.solve(0,0);
 //SETTING PATHWAY COLOR
-for(v=0;v<solution.length;v++){
-document.getElementsByTagName("tr")[solution[v][0]].getElementsByTagName("td")[solution[v][1]].style.backgroundColor="chartreuse"
+for(y=0;y<solution.length;y++){
+document.getElementsByTagName("tr")[solution[y][0]].getElementsByTagName("td")[solution[y][1]].style.backgroundColor="chartreuse"
 }
 }
